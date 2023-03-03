58127f570e292c0a79cfedea9809e0d218fe5437
Victor Zhang <victor.zhang@foxtel.com.au>
2023-03-03 15:08:21 +1100

    cleanup

--- src/Contexts/PlayerContext.tsx
+++ src/Contexts/PlayerContext.tsx
@@ -1,17 +1,9 @@
 import React, { createContext, useContext, useReducer } from 'react';
 import { heroColor, Player, playerNumber, direction, Escalator, DBPlayer } from '../types';
 import cryptoRandomString from 'crypto-random-string';
-import difference from 'lodash/difference';
-
-const playerNumbers = Array.from({length: 8}, (_, i) => i + 1);
 
 export type Action = 
-  // {type: 'playerHeld', value: number | null, color: heroColor} | 
-  // {type: 'showMovableSpaces', value: direction[]} | 
-  // {type: 'showEscalatorSpaces', value: Escalator[]} | 
-  // {type: 'showTeleportSpaces', color: heroColor | null} | 
   {type: 'setPlayer', value: Player | null}
-  // {type: 'assignActions', value: Player} | 
   | undefined;
 export type Dispatch = (action: Action) => void;
 
@@ -23,14 +15,10 @@ export interface PlayerFactoryType {
 }
 
 export const PlayerFactory = (playerName: string, playersInLobby: number) => {
-  // const newPlayerNumber = difference(playerNumbers, currentPlayers)[0];
   const id = cryptoRandomString({length: 10, type: 'alphanumeric'});
 
   const localPlayerState: Player = {
     id,
-    // showMovableDirections: [],
-    // showTeleportSpaces: null,
-    // showEscalatorSpaces: [], // TODO revisit this (trim down data)
   }
 
   const dbPlayerState: DBPlayer = {
@@ -39,7 +27,6 @@ export const PlayerFactory = (playerName: string, playersInLobby: number) => {
     number: playersInLobby + 1 as playerNumber,
     playerDirections: [], 
     playerAbilities: [],
-    // pinged: false
   }
 
   return {
@@ -48,40 +35,23 @@ export const PlayerFactory = (playerName: string, playersInLobby: number) => {
   }
 }
 
-// assign random number
-
-const playerInitialState: Player = {} as Player;
+const playerInitialState: Player | null = null;
 
 const PlayerStateContext = createContext<Player | null | undefined>(undefined);
 const PlayerDispatchContext = createContext<Dispatch | undefined>(undefined);
 
 type PlayerState = Player | null;
 
-const playerReducer = (playerState: PlayerState, action: any) => {
+const playerReducer = (playerState: PlayerState, action: Action) => {
   let newState = {...playerState} as PlayerState;
 
-  switch (action.type) {
-    // case 'playerHeld': {
-    //   return newState;
-    // }
+  switch (action?.type) {
     case 'setPlayer': { // ONLY THIS used
       newState = action.value;
       return newState;
     }
-    // case 'assignActions': {
-    //   // TODO, these are constant values that never change, should move to a seperate provider,
-    //   // so components using these values do not re-render whenever playerContext value changes.
-    //   // MINOR priority
-    //   // newState.playerAbilities = action.value.playerAbilities;
-    //   // newState.playerDirections = action.value.playerDirections;
-    //   return newState;
-    // }
-    // case 'movePawn': {
-    //   // newState[action.color as keyof Player].position = action.value;
-    //   return newState;
-    // }
     default: {
-      throw new Error(`Unhandled action type: ${action.type}`)
+      throw new Error(`Unhandled action type: ${action?.type}`)
     }
   }
 }
